719e9c206bac999b0d1637d5df1fd33268d2b3ef
mgukov <mgookov@gmail.com>
2023-03-07 13:16:45 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module com/mgukov/imageconverter
+
+go 1.18
+
+require github.com/mgukov/goheif v0.0.0-20230306161240-0816d5738559 // indirect
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+
+	heic := os.Args[1]
+	out := os.Args[2]
+
+	if len(heic) == 0 {
+		panic("Empty input file")
+	}
+
+	if len(out) == 0 {
+		panic("Empty output file")
+	}
+
+	log.Println("Convert " + heic + " to " + out)
+
+	err := convertHeicToJpg(heic, out)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Conversion Passed")
+}
